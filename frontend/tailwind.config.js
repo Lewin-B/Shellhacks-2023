a9ea7c65b86275ca2b1f8dbc5824834a885ebd5f
Nathan Chery <nathanchery181@gmail.com>
2023-09-16 13:06:27 -0400

    Added colors to the js file

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -7,8 +7,13 @@ module.exports = {
     extend: {
       backgroundColor: {
         'dark-blue': '#151F42', // Add the custom color with the desired hex code
+        'dark-gray': '#4e5050',
+      },
+      text: {
+        'blue-green': '#58f0ed', // Corrected color value with quotes
       },
     },
   },
   plugins: [],
 }
+

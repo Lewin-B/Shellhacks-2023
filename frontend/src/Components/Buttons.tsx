678b515efa64f5d87a5e95106423d3d4da24ba94
Lewin Bobda <lwbobda@gmail.com>
2023-09-16 15:07:36 -0400

    Activate Upload File Button

--- frontend/src/Components/Buttons.tsx
+++ frontend/src/Components/Buttons.tsx
@@ -1,10 +1,34 @@
+import React, {useState, useRef} from 'react';
 const Buttons: React.FC = () => {
+  const [selectedFile, setSelectedFile] = useState(null);
+  const hiddenFileInput = useRef<HTMLInputElement | null>(null);
+
+
+  // Function to handle file selection
+  const handleFileSelect = (e: React.ChangeEvent<any>) => {
+    const file = e.target.files[0]; // Get the first selected file
+    setSelectedFile(file);
+  };
+  
+  const handleClick = () => {
+    //Click the hidden input
+    if (hiddenFileInput.current) {
+      hiddenFileInput.current.click();
+    }
+
+  }
+
   return (
     <>
       <div className="flex items-center justify-center h-screen">
-        <button className="bg-dark-gray text-blue-300 hover:bg-hover-color font-bold hover:text-white py-2 rounded-full pr-5 pl-5">
+        <button className="bg-dark-gray text-blue-300 hover:bg-hover-color font-bold hover:text-white py-2 rounded-full pr-5 pl-5"
+          onClick={handleClick}>
           Upload Image...
         </button>
+        <input
+          type="file" accept=".jpg, .jpeg, .png, .gif" className='hidden' ref={hiddenFileInput}
+          onChange={handleFileSelect}
+        />
 
         <div className="p-5"></div>
         

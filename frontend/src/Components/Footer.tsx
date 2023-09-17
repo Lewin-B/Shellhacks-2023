d51b0dee32face02b28b014c17bb05bd47c4a651
zach <zwright5270@gmail.com>
2023-09-16 20:13:30 -0400

    added logo to footer

--- frontend/src/Components/Footer.tsx
+++ frontend/src/Components/Footer.tsx
@@ -1,11 +1,17 @@
 import logo from './Logo-1.jpg';
 
+
 const Footer = () => {
     const year = new Date().getFullYear();
     return (
-      <footer style={{ height: '25px', lineHeight: '50px'}}>
-        {`Copyright © Smith.ai ${year}`}
+    <div style={{display: 'flex', justifyContent: 'space-between', margin: '20px'}}>
+      <footer style={{ height: '0px', marginTop: '8px'}}>
+        {`Copyright © BlurrSmith.ai ${year}`}
       </footer>
+      <img src = {logo}
+      style = {{height: '40px', width: '40px', marginRight: '100px', borderRadius: "10%", borderColor: 'black', borderWidth: '2px'}}/>
+    </div>
+
     );
   };
   

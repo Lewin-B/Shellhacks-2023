29662f81f0468d999b509532c2e497d03636fd43
zach <zwright5270@gmail.com>
2023-09-16 21:59:28 -0400

    swapped footer elements

--- frontend/src/Components/Footer.tsx
+++ frontend/src/Components/Footer.tsx
@@ -5,11 +5,11 @@ const Footer = () => {
     const year = new Date().getFullYear();
     return (
     <div style={{display: 'flex', justifyContent: 'space-between', margin: '20px'}}>
+      <img src = {logo}
+      style = {{height: '40px', width: '40px', marginRight: '100px', borderRadius: "10%", borderColor: 'black', borderWidth: '2px'}}/>
       <footer style={{ height: '0px', marginTop: '8px'}}>
         {`Copyright © BlurrSmith.ai ${year}`}
       </footer>
-      <img src = {logo}
-      style = {{height: '40px', width: '40px', marginRight: '100px', borderRadius: "10%", borderColor: 'black', borderWidth: '2px'}}/>
     </div>
 
     );
